9ee1f384d7e02c3ea327bb1f971203139802cdee
CallmeEterHist <kjls515@gmail.com>
2026-08-06 16:40:53 +0900

    오류  수정

--- src/features/profile/mock/profile.ts
+++ src/features/profile/mock/profile.ts
@@ -1,4 +1,4 @@
-import { ROLE } from "@/constants/role";
+import { AUTHORITY } from "@/constants/authority";
 
 import type { MyProfile } from "../types";
 
@@ -6,7 +6,7 @@ import type { MyProfile } from "../types";
 export const MY_PROFILE_MOCK: MyProfile = {
   name: "김서준",
   email: "seojun@techstart.kr",
-  role: ROLE.OWNER,
+  role: AUTHORITY.OWNER,
   companyName: "(주)테크스타트",
   teamName: "제품개발팀",
   position: "수석",
